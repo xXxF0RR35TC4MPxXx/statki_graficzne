f8bcce277e0dac38386504a5e6a1f5cd35968e23
Gabriel <camed.tf@gmail.com>
2022-01-22 18:34:27 +0100

    Update SFMLFactory

--- SFMLFactory.cpp
+++ SFMLFactory.cpp
@@ -5,24 +5,24 @@
 using std::string;
 
 sf::RectangleShape *SFMLFactory::createRectangle(int w, int h) {
-    sf::RectangleShape shape;
-    shape.setSize(sf::Vector2f(w, h));
-    return &shape;
+    auto shape = new sf::RectangleShape();
+    shape->setSize(sf::Vector2f(w, h));
+    return shape;
 }
 
 sf::RectangleShape *SFMLFactory::createRectangle(int w, int h, sf::Texture texture) {
-    sf::RectangleShape shape;
-    shape.setSize(sf::Vector2f(w, h));
-    shape.setTexture(&texture);
-    return &shape;
+    auto shape = new sf::RectangleShape();
+    shape->setSize(sf::Vector2f(w, h));
+    shape->setTexture(&texture);
+    return shape;
 }
 
 sf::Text *SFMLFactory::createText(string val, sf::Font font, int fsize, int xpos, int ypos) {
-    sf::Text t;
-    t.setFont(font);
-    t.setCharacterSize(fsize);
-    t.setPosition(sf::Vector2f(xpos, ypos));
-    t.setString(val);
+    auto t = new sf::Text();
+    t->setFont(font);
+    t->setCharacterSize(fsize);
+    t->setPosition(sf::Vector2f(xpos, ypos));
+    t->setString(val);
 
-    return &t;
+    return t;
 } 
